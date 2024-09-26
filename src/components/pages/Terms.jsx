62b111a91b7e11f634956d564eb899969d7c0148
AdrianGerman <adrian.german1019@gmail.com>
2024-09-26 15:02:41 -0600

    terms and conditions

--- src/components/pages/Terms.jsx
+++ src/components/pages/Terms.jsx
@@ -1,7 +1,105 @@
+import "./Terms.css"
+
 const Terms = () => {
   return (
     <>
-      <h1>Términos y Condiciones</h1>
+      <div className="terms">
+        <h2>Términos y condiciones</h2>
+        <div className="text-terms">
+          <p>
+            <b>Ámbito de aplicación</b>
+          </p>
+
+          <p>
+            Bienvenido a ElMariana Shop y todas sus divisiones, subsidiarias,
+            afiliadas y sitios de internet, la cual te ofrecen acceso al sitio
+            web y te venden productos bajo los términos y condiciones
+            establecidos en la misma, te rogamos que leas detenidamente los
+            presentes términos y condiciones antes de utilizar el sitio web
+            elmariana.shop ya que al entrar a nuestro sitio confirmas tu
+            entendimiento con los mismos, si no aceptas estos términos y
+            condiciones de uso, no podrás utilizar este sitio.
+          </p>
+          <p>
+            A través de su sitio web ElMariana Shop oferta bienes, ofreciendo a
+            sus usuarios y suscriptores la posibilidad de su adquisición, debido
+            al contenido y la finalidad del sitio web, las personas que quieran
+            beneficiarse de sus servicios deben de suscribirse, lo cual se
+            realiza a través del llenado del formulario de registro y siguiendo
+            los pasos que con posterioridad se les comunicará por medio de
+            correo electrónico para dichos efectos, la condición de suscriptor
+            supone la aceptación de los términos y condiciones de uso.
+          </p>
+          <p>
+            Una vez completado el formulario de registro, el suscriptor del
+            sitio recibirá una contraseña de acceso, el suscriptor será el único
+            responsable de mantener la contraseña proporcionada y así como otros
+            identificadores de cuentas seguras, el titular de la cuenta es
+            totalmente responsable de todas las actividades que ocurran bajo su
+            contraseña o cuenta, por otra parte, debes notificarnos de cualquier
+            uso no autorizado de tu contraseña o cuenta, de ninguna manera,
+            nosotros seremos responsables, directos o indirectamente, por
+            cualquier pérdida o daño de cualquier tipo incurrido como resultado
+            de la falta de cumplimiento de esta condición.
+          </p>
+          <p>
+            ElMariana Shop se reserva el derecho de cambiar, modificar, añadir o
+            eliminar partes de estos términos y condiciones de uso en cualquier
+            momento, los cambios serán efectivos cuando se publiquen en el sitio
+            elmariana.shop por favor revisa periódicamente las actualizaciones
+            de estos términos y condiciones de uso, ya que tendrás que aceptar
+            los cambios que se realicen para poder seguir accediendo al sitio y
+            seguir comprando productos.
+          </p>
+
+          <p>
+            <b>Uso de ElMariana Shop</b>
+          </p>
+
+          <p>
+            Para poder utilizar el sitio elmariana.shop debes de tener 18 años
+            de edad o en su caso estar accediendo bajo la supervisión y
+            consentimiento de tus padres, de quien detente el legar ejercicio de
+            la patria potestad, guarda y custodia o bien de tutor legal si es el
+            caso. Cada usuario sólo podrá ser titular de 1 (una) cuenta en el
+            sitio elmariana.shop no pudiendo acceder a más de 1 (una) cuenta en
+            el sitio web con distintas direcciones de correo electrónico o
+            falseando, modificando y/o alterando sus datos personales de
+            cualquier manera posible. Si se verificará o sospechará un uso
+            fraudulento y/o malintencionado y/o contrario a estos Términos y
+            Condiciones y/o contrarios a la buena fe, ElMariana Shop tendrá el
+            derecho inapelable de dar por terminada la relación, dar de baja las
+            cuentas y hasta colaborar judicialmente para la persecución a los
+            infractores. Al aceptar los presentes términos y condiciones te
+            concedemos una licencia no transferible y revocable para utilizar
+            nuestro sitio, en virtud de los términos y condiciones descritos y
+            aceptados, con el propósito de la compra de artículos ofertados en
+            nuestra página.
+          </p>
+          <p>
+            Al aceptar los presentes términos y condiciones te concedemos una
+            licencia no transferible y revocable para utilizar nuestro sitio, en
+            virtud de los términos y condiciones descritos y aceptados, con el
+            propósito de la compra de artículos ofertados en nuestra página.
+          </p>
+          <p>
+            El uso comercial o el nombre de terceros están prohibidos, salvo lo
+            expresamente permitido por nosotros con anterioridad, cualquier
+            infracción de estos términos y condiciones dará lugar a la
+            revocación inmediata de la licencia otorgada en este apartado, sin
+            previo aviso.
+          </p>
+          <p>
+            Ciertos servicios y las características relacionadas con los mismos,
+            que pueden estar disponibles en el sitio elmariana.shop, pueden
+            requerir el registro o suscripción, si decides registrarte o
+            suscribirte a cualquiera de estos servicios o funciones
+            relacionadas, te comprometes a proporcionar información precisa y
+            actualizada acerca de ti mismo, para actualizar rápidamente esa
+            información si hay algún cambio.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
