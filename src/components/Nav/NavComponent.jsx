605c912bc063aa64404159bdf7ddf86a982fa59e
AdrianGerman <adrian.german1019@gmail.com>
2024-09-27 09:45:17 -0600

    responsive design and font web

--- src/components/Nav/NavComponent.jsx
+++ src/components/Nav/NavComponent.jsx
@@ -157,11 +157,11 @@ const NavComponent = () => {
           </svg>
 
           <svg
+            className="none"
             width="28"
             height="28"
             viewBox="0 0 24 24"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
             color="#000000"
           >
             <path
