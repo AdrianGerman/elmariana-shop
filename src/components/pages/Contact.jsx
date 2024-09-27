4f62bfd554306094bc9bb3b29012c1ccf98bba2c
AdrianGerman <adrian.german1019@gmail.com>
2024-09-27 09:22:33 -0600

    contact and font bold

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,45 @@
+import "./Contact.css"
+
 const Contact = () => {
   return (
     <>
-      <h1>! Ponte en contacto con nosotros !</h1>
+      <div className="contact">
+        <div className="contact-text">
+          <h2>! Ponte en contacto con nosotros !</h2>
+          <p>
+            Si tienes alguna duda o sugerencia, o necesitas apoyo con alguna
+            compra, puedes escribirnos un mensaje y te responderemos lo mas
+            pronto posible.
+          </p>
+          <h6>
+            <strong>Lun - Vie: 10am - 6pm</strong> <br />
+            shop@elmariana.shop
+          </h6>
+        </div>
+
+        <div className="form-contact">
+          <section className="info-me-contact">
+            <input type="text" placeholder="Nombre" className="input-field" />
+            <input
+              type="text"
+              placeholder="Correo electrónico *"
+              className="input-field"
+            />
+          </section>
+          <section className="more-info-contact">
+            <input
+              type="text"
+              placeholder="Número de teléfono"
+              className="input-field"
+            />
+            <textarea
+              placeholder="Comentario"
+              className="input-field"
+            ></textarea>
+          </section>
+          <button className="submit-btn">Enviar</button>
+        </div>
+      </div>
     </>
   )
 }
